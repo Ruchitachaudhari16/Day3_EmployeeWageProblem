3c6830bcdff4d4b0fd9eb4bef09f929ddbdce0fb
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-17 17:42:22 -0400

    [Ruchita] Add: Employee Wages Using Switch case

--- EmployeeWage/EmployeeWage/Program.cs
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -10,9 +10,8 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            //UC3:- Add
-            //Part time Employee Wage 
-            EmployeeAttendance.EmployeeWage();
+            //UC4:- EmployeeWages using Switch case
+            EmployeeAttendance.EmployeeWagesCalculation();
             Console.ReadLine();
         }
     }
